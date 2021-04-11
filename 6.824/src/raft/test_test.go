25255bac8b40253e6c17fb43013365dcbaedba4f
yyeatgrass <344588135z@gmail.com>
2021-04-11 04:08:53 -0700

    Fix index check issue in TestRPCBytes2B

--- 6.824/src/raft/test_test.go
+++ 6.824/src/raft/test_test.go
@@ -161,7 +161,7 @@ func TestRPCBytes2B(t *testing.T) {
 	for index := 2; index < iters+2; index++ {
 		cmd := randstring(5000)
 		xindex := cfg.one(cmd, servers, false)
-		if xindex != index {
+		if xindex != index - 1 {
 			t.Fatalf("got index %v but expected %v", xindex, index)
 		}
 		sent += int64(len(cmd))
